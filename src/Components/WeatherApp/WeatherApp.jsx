a7c60730c836d229fe94efa74f070f7fd9ef4c2c
Kenny Lai <3154018+kenny791@users.noreply.github.com>
2023-11-27 22:17:31 +1100

    add search api string to search function

--- src/Components/WeatherApp/WeatherApp.jsx
+++ src/Components/WeatherApp/WeatherApp.jsx
@@ -13,12 +13,21 @@ export const WeatherApp = () => {
   
     let api_key = "87b173ea22b2e0af751832b026578384"
     
+    const search = () => {
+        const element = document.getElementsByClassName("cityInput")
+        if(element[0].value === ""){
+            return 0
+        }
+    
+        let url =  `https://api.openweathermap.org/data/2.5/weather?q=${element[0].value}&units=Metric&appid=${api_key}`
+        
+    }
     
     return (
     <div className='container'>
         <div className="top-bar">
             <input type="text" className="cityInput" placeholder='Search'/>
-            <div className="search-icon">
+            <div className="search-icon" onClick={()=>{search()}}>
                 <img src={search_icon} alt="search"/>
             </div>
         </div>
